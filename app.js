cda6292be5fb747b809631748e29a2cbe2095ea0
GrantGeter <72525074+GrantGeter@users.noreply.github.com>
2021-11-23 13:07:06 -0600

    build script

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const port = process.env.PORT || 80
 
 app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
-app.listen(3000, () =>
-    console.log('Visit http://127.0.0.1:3000')
+app.listen(port, () =>
+    console.log('Up and running')
 );
