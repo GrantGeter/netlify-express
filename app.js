d0617199b4c7f58a93da100f477708b01c57618c
GrantGeter <72525074+GrantGeter@users.noreply.github.com>
2021-11-23 19:14:36 -0600

    cors

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const path = require('path')
 const port = process.env.PORT || 80
+
+app.use(cors());
+app.options('*', cors());
 
 app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
