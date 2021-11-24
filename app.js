0eb2718949243e8982f759a02f6c1a4410530e9a
GrantGeter <72525074+GrantGeter@users.noreply.github.com>
2021-11-23 19:21:35 -0600

    cors

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const cors = require('cors');
 const path = require('path')
 const port = process.env.PORT || 80
 
-app.use(cors());
-app.options('*', cors());
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
